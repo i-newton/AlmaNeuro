d204c34a8989fb87db21198c5e1894d07202b5dc
kairat_kushaev <kkushaev@mirantis.com>
2016-11-30 22:08:12 +0300

    Add some fixes for linear regression

--- samples/neuronets/linear_classifier.py
+++ samples/neuronets/linear_classifier.py
@@ -7,67 +7,42 @@ from utils import vec_utils
 class LinearClassifier(base_net.BaseNet):
     def __init__(self, input_dim, output_dim):
         super(LinearClassifier, self).__init__(input_dim, output_dim)
-        self.weights = [
-            [random.random() for i in range(input_dim + 1)]
-            for j in range(output_dim)
-        ]
+        self.weights = [[0.0 for i in range(input_dim + 1)] for j in range(output_dim)]
 
-<<<<<<< HEAD
-    STEP = 0.2
+    STEP = 0.02
 
     def learn(self, input_vecs, result_vecs, error_threshold=0.01,
-              max_iterations=1000):
+              max_iterations=10000):
         # for each output classifier we learn all examples
         for output_num in range(self.output_dim):
             for iter in range(max_iterations):
-=======
-    STEP = 0.1
-
-    def learn(self, input_vecs, result_vecs, error_threshold=0.01,
-              max_iterations=1000):
-        current_iter = 0
-        # for each output classifier we learn all examples
-        for output_num in range(self.output_dim):
-            while current_iter < max_iterations:
->>>>>>> f1feff5b2a703dae90a726497e285d2d8c3ccf73
                 # for each target vec we need to calculate errors over all
                 # input samples
                 satisfied = True
                 # deltas is mean diff gradient for each weight
-                deltas = [0.0 for i in range(self.input_dim + 1)]
+                grads = [0.0 for i in range(self.input_dim + 1)]
                 # compare each sample with desired output
                 for sample_input, desired_output in zip(
                         input_vecs, result_vecs):
                     # calculate actual output for sample input
                     actual = self.get_output(sample_input, output_num)
                     # compare with desired output and calculate error
-<<<<<<< HEAD
                     error = desired_output[output_num] - actual
-=======
-                    error = actual - desired_output[output_num]
->>>>>>> f1feff5b2a703dae90a726497e285d2d8c3ccf73
-                    if (error**2)/2 > error_threshold:
+                    if (error*error)/2 > error_threshold:
                         satisfied = False
                     # add error gradient for this actual output to
                     # overall gradient
-                    deltas[0] += error/len(input_vecs)
-<<<<<<< HEAD
-                    for k in range(self.input_dim):
-                        deltas[k + 1] += sample_input[k] * error/len(input_vecs)
-=======
-                    for k in range(1, self.input_dim + 1):
-                        deltas[k] += sample_input[k] * error/len(input_vecs)
->>>>>>> f1feff5b2a703dae90a726497e285d2d8c3ccf73
+                    for k, sample in enumerate([1] + sample_input):
+                        grads[k] += sample * error
                 if satisfied:
                     break
                 else:
                     for weight_num in range(self.input_dim + 1):
                         self.weights[output_num][weight_num] += \
-                            self.STEP * deltas[weight_num]
+                            (self.STEP * grads[weight_num])/len(input_vecs)
 
     def get_output(self, input_vec, vec_num):
-        return self.weights[vec_num][0] + vec_utils.multiply(
-            input_vec, self.weights[vec_num][1:])
+        return vec_utils.multiply([1] + input_vec, self.weights[vec_num])
 
     def get_results(self, input_vec):
         return [self.get_output(input_vec, i) for i in range(self.output_dim)]
--- tests/simple_test.py
+++ tests/simple_test.py
@@ -4,13 +4,14 @@ if __name__ == '__main__':
     net = linear_classifier.LinearClassifier(2, 1)
     input_samples = [[1, 1], [0, 0], [2, 2], [3, 3]]
     output_samples = [[1], [0], [2], [3]]
-    net.learn(input_samples, output_samples, error_threshold=0.1,
-              max_iterations=50)
-    print net.get_results([4, 4])
+    net.learn(input_samples, output_samples, error_threshold=0.00001)
+    print(net.get_results([4, 4]))
 
     net2 = linear_classifier.LinearClassifier(1, 1)
-    input_samples = [[1], [0], [2], [3]]
-    output_samples = [[1], [0], [2], [3]]
-    net2.learn(input_samples, output_samples, error_threshold=0.1,
-               max_iterations=50)
-    print net2.get_results([4])
+    input_samples = []
+    output_samples = []
+    for i in range(10):
+        input_samples.append([i])
+        output_samples.append([i-1])
+    net2.learn(input_samples, output_samples, error_threshold=0.00001)
+    print(net2.get_results([-1]))
